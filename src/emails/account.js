5c2c060cd223d6bff71e7eb0143730386bc36a29
{MinhBienK2} <{phamminhbien333@gmail.com}>
2022-01-28 00:07:47 +0700

    updateheheheh

--- src/emails/account.js
+++ src/emails/account.js
@@ -6,7 +6,7 @@ const pass = process.env.PASSWORD
 let transporter = nodemailer.createTransport({
     host: "smtp.gmail.com",
     port: 587,
-    secure: false, // true for 465, false for other ports
+   // secure: false, // true for 465, false for other ports
     auth: {
         user, // generated ethereal user
         pass  // generated ethereal password
