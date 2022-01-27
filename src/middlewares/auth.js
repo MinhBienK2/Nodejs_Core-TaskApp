7be52a79d15935ea40b9410cf257666a968a4bec
{MinhBienK2} <{phamminhbien333@gmail.com}>
2022-01-27 18:39:51 +0700

    update fixbug

--- src/middlewares/auth.js
+++ src/middlewares/auth.js
@@ -4,7 +4,7 @@ const jwt = require('jsonwebtoken');
 const auth = async (req,res,next) => {
     try{
         const token = req.get('authorization').replace('Bearer ','')
-        const decode =await jwt.verify(token,process.env.JWT_SECRE)
+        const decode =await jwt.verify(token,process.env.JWT_SECRET)
         const user =await User.findOne({_id : decode._id,'tokens.token':token})
         if(!user){
             throw new Error('Please authorization !')
