bf075e91f42b79794b45b201364e47ca76c00aa5
{MinhBienK2} <{phamminhbien333@gmail.com}>
2022-01-27 19:22:40 +0700

    update gmail

--- src/emails/account.js
+++ src/emails/account.js
@@ -3,14 +3,17 @@ const nodemailer = require("nodemailer");
 let transporter = nodemailer.createTransport({
     service : 'gmail',
     auth: {
-        user : process.env.USER_NAME, // generated ethereal user
-        pass : process.env.PASSWORD // generated ethereal password
+        // user : process.env.USER_NAME, // generated ethereal user
+        // pass : process.env.PASSWORD // generated ethereal password
+        user : 'phamminhbien3333@gmail.com', // generated ethereal user
+        pass : 'phamminhbien123' // generated ethereal password
     },
 });
 
 const sendWelcomeMessage =async (email,name) => {
     let info = await transporter.sendMail({
-        from: `"CLUB MARVEL" < ${process.env.USER_NAME} >`, // sender address
+        // from: `"CLUB MARVEL" < ${process.env.USER_NAME} >`, // sender address
+        from: `"CLUB MARVEL" < phamminhbien3333@gmail.com >`, // sender address
         to: email, // list of receivers
         subject: "MARVEL", // Subject line
         text: `welcom ${name} to MARVEL ! Wish you a good exprerience ^.^ `, // plain text body
@@ -22,7 +25,8 @@ const sendWelcomeMessage =async (email,name) => {
 
 const sendCancelationEmail = async (email,name) => {
     let info = await transporter.sendMail({
-        from: `"CLUB MARVEL" < ${process.env.USER_NAME} >`, // sender address
+        // from: `"CLUB MARVEL" < ${process.env.USER_NAME} >`, // sender address
+        from: `"CLUB MARVEL" < phamminhbien3333@gmail.com >`, // sender address
         to: email, // list of receivers
         subject: "MARVEL", // Subject line
         text: `What is your matter ? ${name} . Hope you come back soon T_T `, // plain text body
